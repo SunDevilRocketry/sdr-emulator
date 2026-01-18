be5e0f8560de2e60b239cda16cc89293117fc89e
ETSsound <147210601+ETSsound@users.noreply.github.com>
2026-01-18 16:44:03 -0700

    Add a temporary interface to indicate LED colors

--- src/emulator_gpio.c
+++ src/emulator_gpio.c
@@ -24,17 +24,50 @@
 ------------------------------------------------------------------------------*/
 #include <string.h>
 #include <stdint.h>
+#include <stdio.h>
 
 #include "emulator.h"
 #include "main.h"
 #include "stm32h7xx_hal.h"
+#include "sdr_pin_defines_A0002.h"
 
 /*------------------------------------------------------------------------------
  HAL interfaces                                                       
 ------------------------------------------------------------------------------*/
 
 void HAL_GPIO_WritePin(GPIO_TypeDef *GPIOx, uint16_t GPIO_Pin, GPIO_PinState PinState) {
-    return;
+    if( GPIOx == STATUS_GPIO_PORT )
+        {
+        // ETS TEMP: Replace with IPC to GUI
+        printf("LED color changed to: ");
+        switch (GPIO_Pin) 
+            {
+            case STATUS_G_PIN:
+                printf("GREEN\n");
+                break;
+            case STATUS_R_PIN:
+                printf("RED\n");
+                break;
+            case STATUS_B_PIN:
+                printf("BLUE\n");
+                break;
+            case STATUS_G_PIN | STATUS_R_PIN:
+                printf("YELLOW\n");
+                break;
+            case STATUS_G_PIN | STATUS_B_PIN:
+                printf("CYAN\n");
+                break;
+            case STATUS_R_PIN | STATUS_B_PIN:
+                printf("PURPLE\n");
+                break;
+            case STATUS_R_PIN | STATUS_G_PIN | STATUS_B_PIN:
+                printf("WHITE\n");
+                break;
+            default:
+                printf("-=Indeterminate=-\n");
+                break;
+            }
+        }
 }
 
 GPIO_PinState HAL_GPIO_ReadPin(const GPIO_TypeDef *GPIOx, uint16_t GPIO_Pin) {
