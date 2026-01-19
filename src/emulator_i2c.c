ea918f1aeb64397a8868991e2baf901c8157f7b9
ETSsound <147210601+ETSsound@users.noreply.github.com>
2026-01-19 16:22:47 -0700

    Fix baro init time errors

--- src/emulator_i2c.c
+++ src/emulator_i2c.c
@@ -27,6 +27,14 @@
 
 #include "emulator.h"
 #include "stm32h7xx_hal.h"
+#include "baro.h"
+#include "sdr_pin_defines_A0002.h"
+
+/*------------------------------------------------------------------------------
+ Procedure prototypes                                                       
+------------------------------------------------------------------------------*/
+static HAL_StatusTypeDef baro_read_handler(I2C_HandleTypeDef *hi2c, uint16_t DevAddress, uint16_t MemAddress,
+                                    uint16_t MemAddSize, uint8_t *pData, uint16_t Size, uint32_t Timeout);
 
 /*------------------------------------------------------------------------------
  HAL interfaces                                                       
@@ -39,6 +47,11 @@ HAL_StatusTypeDef HAL_I2C_Mem_Write(I2C_HandleTypeDef *hi2c, uint16_t DevAddress
 
 HAL_StatusTypeDef HAL_I2C_Mem_Read(I2C_HandleTypeDef *hi2c, uint16_t DevAddress, uint16_t MemAddress,
                                     uint16_t MemAddSize, uint8_t *pData, uint16_t Size, uint32_t Timeout) {
+if( hi2c == &( BARO_I2C ) )
+    {
+    return baro_read_handler( hi2c, DevAddress, MemAddress, MemAddSize, pData, Size, Timeout);
+    }
+
     return HAL_OK;
 }
 
@@ -50,3 +63,22 @@ HAL_StatusTypeDef HAL_I2C_Mem_Read_IT(I2C_HandleTypeDef *hi2c, uint16_t DevAddre
 /*------------------------------------------------------------------------------
  Procedures                                                     
 ------------------------------------------------------------------------------*/
+static HAL_StatusTypeDef baro_read_handler(I2C_HandleTypeDef *hi2c, uint16_t DevAddress, uint16_t MemAddress,
+                                    uint16_t MemAddSize, uint8_t *pData, uint16_t Size, uint32_t Timeout)
+{
+    /* imperfect approximation, but delay a bit like the real FC */
+    HAL_Delay( (uint8_t)(0.4 * Size) );
+
+    if( MemAddress == BARO_REG_CHIP_ID )
+        {
+        *pData = BMP390_DEVICE_ID;
+        return HAL_OK;
+        }
+    if( MemAddress == BARO_REG_ERR_REG )
+        {
+        *pData = 0x00; /* Clear the error register */
+        return HAL_OK;
+        }
+    
+    return HAL_OK;
+}
