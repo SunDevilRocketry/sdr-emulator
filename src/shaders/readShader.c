ceb7576632a38e0ccaee7d334e7e1ed30d239f9b
butchhartman <rcamacho5436@gmail.com>
2026-02-22 10:17:24 -0700

    Standards pass

--- src/shaders/readShader.c
+++ src/shaders/readShader.c
@@ -1,4 +1,28 @@
-#include "shaders/readShader.h"
+/*******************************************************************************
+*
+* FILE: 
+* 		readShader.c
+*
+* DESCRIPTION: 
+* 		Implementations of shader handling functions
+*       
+*                                                                             
+* COPYRIGHT:                                                                  
+*       Copyright (c) 2026 Sun Devil Rocketry.                                
+*       All rights reserved.                                                  
+*                                                                             
+*       This software is licensed under terms that can be found in the LICENSE
+*       file in the root directory of this software component.                 
+*       If no LICENSE file comes with this software, it is covered under the   
+*       BSD-3-Clause.                                                          
+*                                                                              
+*       https://opensource.org/license/bsd-3-clause                            
+*
+*******************************************************************************/
+
+/*------------------------------------------------------------------------------
+ Includes                                                         
+------------------------------------------------------------------------------*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/fcntl.h>
@@ -7,6 +31,23 @@
 #include <errno.h>
 #include <unistd.h>
 
+#include "shaders/readShader.h"
+
+/*------------------------------------------------------------------------------
+ Procedures                                                     
+------------------------------------------------------------------------------*/
+
+/*******************************************************************************
+*                                                                              *
+* PROCEDURE:                                                                   * 
+* 		readShaderSource                                                       *
+*                                                                              *
+* DESCRIPTION:                                                                 * 
+*       Reads the file at the given path and returns a NULL-terminated string  *
+*       containing that file's contents. File must exist and caller is         * 
+*       responsible for freeing the returned string.                           *
+*                                                                              *
+*******************************************************************************/
 const char* readShaderSource
     (
     const char* path
@@ -39,8 +80,11 @@ data[stat.st_size] = '\0'; // null term
 
 if (bytesRead != stat.st_size || bytesRead == -1) {
     fprintf(stderr, "Bad read of shader file %s with errno %d\n", path, errno);
+    free(data);
+    return NULL;
 }
 
 return data;
-}
+
+} /* readShaderSource */
 
