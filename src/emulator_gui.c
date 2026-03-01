1e19ab698fb40697b58a1d28d45934ac8a78acb8
butchhartman <rcamacho5436@gmail.com>
2026-03-01 12:28:10 -0700

    Made window invisible at creation time

--- src/emulator_gui.c
+++ src/emulator_gui.c
@@ -91,6 +91,7 @@ glfwSetErrorCallback(error_callback);
 glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
 glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
 glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE);
 guiWindow = glfwCreateWindow(640, 480, "SDR HW Emulator", NULL, NULL);
 glfwHideWindow(guiWindow);
 
