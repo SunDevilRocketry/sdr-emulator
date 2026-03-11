0991f07d9e7cf969120f02471f47660c60ea6ca2
butchhartman <rcamacho5436@gmail.com>
2026-03-10 17:21:02 -0700

    Reformatted the dynamic array files

--- src/containers/darr.c
+++ src/containers/darr.c
@@ -1,22 +1,42 @@
 #include "containers/darr.h"
+#include <stdlib.h>
 
-void _DARRAY_GROW_FUNC(void** array) {
-//        printf("ARRAY ADDRE: %p\n", *array);
- //       printf("GROW_HEADER ADDRE: %p\n", *array);
-        _DARRAY_GET_HEADER(*array)->arrCapacity = (_DARRAY_GET_HEADER(*array)->arrCapacity > 0) ? _DARRAY_GET_HEADER(*array)->arrCapacity * DARRAY_GROWTH_FACTOR : DARRAY_INITIAL_CAPACITY;
-        void* tmp = realloc(_DARRAY_GET_HEADER(*array), _DARRAY_GET_HEADER(*array)->elementSize * _DARRAY_GET_HEADER(*array)->arrCapacity + sizeof(struct _darray_secret_header));
+/**
+ * @brief Grows the passed dynamic array by the growth factor. Initialized to initial capacity if empty.
+ *
+ * @warning Avoid calling this without using the macro
+ */
+void _darray_grow_func(void** array) {
+        struct _darray_secret_header* arrayHeader = _DARRAY_GET_HEADER(*array);
+        if ( arrayHeader->arrCapacity > 0)
+            {
+            arrayHeader->arrCapacity *= DARRAY_GROWTH_FACTOR; 
+            }
+        else
+            {
+            arrayHeader->arrCapacity = DARRAY_INITIAL_CAPACITY;
+            }
+
+        void* tmp = realloc(arrayHeader, arrayHeader->elementSize * arrayHeader->arrCapacity + sizeof(struct _darray_secret_header));
         if (tmp == NULL) {
                 free(*array);
                 *array = NULL;
+                abort();
                 return;
         }
         
         *array = ((struct _darray_secret_header*)tmp) + 1;
 }
 
-void* _DARRAY_NEW_FUNC(size_t sizeInBytes, size_t capacity, size_t elementSize) {
-        // This is never zero, as the size of the header will always be allocated
+/**
+ * @brief creates and returns a new dynamic array
+ *
+ * @warning Avoid calling this without using the macro
+ */
+void* _darray_new_func(size_t sizeInBytes, size_t capacity, size_t elementSize) {
+        /* This is never zero, as the size of the header will always be allocated */
         void* allocation = malloc(sizeInBytes);
+
         if ( allocation == NULL ) {
                 return NULL;
         }
